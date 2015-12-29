995bd062afb500c51df8247e4f47e8e2a846c15d
cch5ng <cch5ng@gmail.com>
2015-12-28 18:48:42 -0800

    fix: add track 5 functions

--- client/playground.js
+++ client/playground.js
@@ -11,16 +11,22 @@ maxim1 = new Maxim();
 maxim2 = new Maxim();
 maxim3 = new Maxim();
 maxim4 = new Maxim();
+maxim5 = new Maxim();
+maxim6 = new Maxim();
+maxim7 = new Maxim();
+maxim8 = new Maxim();
 
 //new player per Maxim obj
 player1 = maxim1.loadFile("drums1.wav");
-player1.loop
+player1.loop;
 player2 = maxim2.loadFile("bassline.wav");
 player2.loop;
 player3 = maxim3.loadFile("arp.wav");
 player3.loop;
 player4 = maxim4.loadFile("vibes.wav");
 player4.loop;
+player5 = maxim5.loadFile("snaredrum1.wav");
+player5.loop;
 
 playDrums = function(){
 	player1.volume(1);
@@ -70,11 +76,24 @@ setVibesVolume = function(vol) {
 	player4.volume(vol);
 };
 
+playSnaredrum = function(){
+	player5.volume(1);
+};
+
+stopSnaredrum = function(){
+	player5.volume(0);
+};
+
+setSnaredrumVolume = function(vol) {
+	player5.volume(vol);
+};
+
 playAll = function() {
 	player1.play();
 	player2.play();
 	player3.play();
 	player4.play();
+	player5.play();
 };
 
 stopAll = function() {
@@ -82,6 +101,7 @@ stopAll = function() {
 	player2.stop();
 	player3.stop();
 	player4.stop();
+	player5.stop();
 };
 
 setSpeed = function(speed) {
@@ -89,6 +109,7 @@ setSpeed = function(speed) {
 	player2.speed(speed);
 	player3.speed(speed);
 	player4.speed(speed);
+	player5.speed(speed);
 };
 
 
