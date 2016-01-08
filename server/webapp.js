6523c614d7d461ffdc8ae454d02ee65491e7e6fc
cch5ng <cch5ng@gmail.com>
2016-01-07 16:17:56 -0800

    fix: update browser policy

--- server/webapp.js
+++ server/webapp.js
@@ -5,48 +5,50 @@ var connectHandler = WebApp.connectHandlers; // get meteor-core's connect-implem
 Meteor.startup(function () {
 
 	//TEST security policies
-	BrowserPolicy.content.allowInlineStyles();
-	BrowserPolicy.content.allowFontDataUrl();
-	BrowserPolicy.content.allowMediaOrigin('*.meteor.com');
-	BrowserPolicy.content.allowMediaSameOrigin();
-	//BrowserPolicy.content.allowSameOriginForAll();
-//allows permissions to get external resources, but not working as well for specific content types
-//i.e. allowFontOrigin(origin) is not working whereas allowOriginForAll(origin) does work
-	BrowserPolicy.content.allowOriginForAll('*.meteor.com', 'https://maxcdn.bootstrapcdn.com', 'code.jquery.com', 'https://fonts.googleapis.com', 'ajax.googleapis.com');
-	BrowserPolicy.content.allowOriginForAll('ajax.googleapis.com');
-	BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
-	BrowserPolicy.content.allowOriginForAll('https://maxcdn.bootstrapcdn.com');
-	BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
-	BrowserPolicy.content.allowConnectOrigin("*.meteor.com");
-
 	// Listen to incoming HTTP requests, can only be used on the server
 	connectHandler.use("/public", function(req, res, next) {
 		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.setHeader("Access-Control-Allow-Methods", "GET");
-		res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, X-Requested-With, Origin');
+		//res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, X-Requested-With, Origin');
 		//return next();
 	});
+
+	BrowserPolicy.content.allowInlineStyles();
+	BrowserPolicy.content.allowFontDataUrl();
+	BrowserPolicy.content.allowMediaSameOrigin();
+	//BrowserPolicy.content.allowSameOriginForAll();
+//allows permissions to get external resources, but not working as well for specific content types
+//i.e. allowFontOrigin(origin) is not working whereas allowOriginForAll(origin) does work
+	BrowserPolicy.content.allowOriginForAll('*.meteor.com');
+	BrowserPolicy.content.allowOriginForAll('ajax.googleapis.com');
+	BrowserPolicy.content.allowOriginForAll('code.jquery.com');
+	BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
+	BrowserPolicy.content.allowOriginForAll('https://maxcdn.bootstrapcdn.com');
+	BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
+	BrowserPolicy.content.allowMediaOrigin('chc_machina_musica2.meteor.com');
+	BrowserPolicy.content.allowConnectOrigin("*.meteor.com");
+
 	//END TEST SECURITY
 
 	//TEST
 	Meteor.methods({
-	  getAudio: function (filename) {
-		console.log('this should make xhr request to ' + filename);
+		getAudio: function (filename) {
+			console.log('this should make xhr request to ' + filename);
 
-		var url = 'http://chc_machina_musica2.meteor.com/' + filename;
-		console.log('url: ' + url);
-		//var result = 
-		HTTP.call("GET", url, {responseType: 'arraybuffer'}, function(error, result) {
-			if (!error) {
-				console.log('type of: ' + typeof result.content);
-				return result;
-			} else {
-				console.log('error: ' + error);
-			}
+			var url = 'http://chc_machina_musica2.meteor.com/' + filename;
+			console.log('url: ' + url);
+			//var result = 
+			HTTP.call("GET", url, {responseType: 'arraybuffer'}, function(error, result) {
+				if (!error) {
+					console.log('type of: ' + typeof result.content);
+					return result;
+				} else {
+					console.log('error: ' + error);
+				}
 
-		});
+			});
 		//return "some return value";
-	  }//,
+		}//,
 	});
 
 
