07ca0059845c759c0736fec690b7079f11285f49
cch5ng <cch5ng@gmail.com>
2016-01-12 16:54:37 -0800

    fix: try fix CORS

--- server/webapp.js
+++ server/webapp.js
@@ -1,32 +1,28 @@
 //server/webapp.js
 
-var connectHandler = WebApp.connectHandlers; // get meteor-core's connect-implementation
+//var connectHandler = WebApp.connectHandlers; // get meteor-core's connect-implementation
 
 Meteor.startup(function () {
 
 	//TEST security policies
-	// Listen to incoming HTTP requests, can only be used on the server
-	connectHandler.use("/public", function(req, res, next) {
-		res.setHeader('Access-Control-Allow-Origin', '*');
-		res.setHeader("Access-Control-Allow-Methods", "GET");
-		//res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type, X-Requested-With, Origin');
+
+	WebApp.connectHandlers.use(function(req, res, next) {
+		res.setHeader("Access-Control-Allow-Origin", "*");
 		return next();
 	});
 
-	BrowserPolicy.content.allowInlineStyles();
-	BrowserPolicy.content.allowFontDataUrl();
+	BrowserPolicy.content.allowInlineScripts();
+	BrowserPolicy.content.allowFontOrigin('chc_machina_musica.meteor.com');
+	BrowserPolicy.content.allowScriptOrigin('chc_machina_musica.meteor.com');
 	BrowserPolicy.content.allowMediaSameOrigin();
-	//BrowserPolicy.content.allowSameOriginForAll();
-//allows permissions to get external resources, but not working as well for specific content types
-//i.e. allowFontOrigin(origin) is not working whereas allowOriginForAll(origin) does work
+	BrowserPolicy.content.allowSameOriginForAll();
 	BrowserPolicy.content.allowOriginForAll('*.meteor.com');
 	BrowserPolicy.content.allowOriginForAll('ajax.googleapis.com');
 	BrowserPolicy.content.allowOriginForAll('code.jquery.com');
 	BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
 	BrowserPolicy.content.allowOriginForAll('https://maxcdn.bootstrapcdn.com');
 	BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
-	BrowserPolicy.content.allowMediaOrigin('chc_machina_musica2.meteor.com');
-	BrowserPolicy.content.allowConnectOrigin("*.meteor.com");
+	BrowserPolicy.content.allowMediaOrigin('chc_machina_musica.meteor.com');
 
 	//END TEST SECURITY
 
